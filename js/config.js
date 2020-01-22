cbfd64ddbbaff04adcd5d27a657f3ee21be80a81
방준영 <youngblood0225@gmail.com>
2020-01-22 10:43:32 +0900

    S02P12C103-26 [+] face&object recognition module

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ angular.module('config', [])
   .constant('config',
   {
     prod: {
-      region: 'ap-northeast-1',
-      upload_bucket_name: 'your_bucket_name',
-      identity_pool_id: 'ap-northeast-1:your_pool_id',
+      region: 'ap-northeast-2',
+      upload_bucket_name: 'jytestbucket79',
+      identity_pool_id: 'ap-northeast-2_hs8UJAsfo',
       face_collection: 'rekognition-demo-go',
       ddb_table: 'rekognition-demo-go'
 
