b6dfd94b4b2b43090b43054408cda9a85ba818d6
junyoung <youngblood0225@gmail.com>
2020-01-30 17:43:11 +0900

    S02P12C103-20{+] 메뉴 클릭시 장바구니에 담김(기능만)

--- web/src/pages/test/test1_db.jsx
+++ web/src/pages/test/test1_db.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../layout/Layout";
 import burger from "../../img/burger.jpg";
 import chicken from "../../img/chicken.jpg";
@@ -10,63 +10,83 @@ import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 
-class Product extends React.Component {
-  handleClick(name) {
-    console.log("click!!! ==>> " + name);
-  }
-  render() {
-    return (
-      <span className="listtest">
-        <TableCell style={{ textAlign: "center" }}>
-          <img
-            src={this.props.src}
-            alt="menu"
-            style={{ width: 250 }}
-            onClick={() => this.handleClick(this.props.name)}
-          />
-          <br></br>
-          {this.props.name}
-        </TableCell>
-      </span>
-    );
-  }
-}
-const products = [
-  { key: "1", src: burger, name: "햄버거임", price: "6000" },
-  { key: "2", src: chicken, name: "치킨임", price: "17000" },
-  { key: "3", src: coke, name: "콜라임", price: "2500" },
-  { key: "4", src: set, name: "세트묵자", price: "20000" }
+import Button from "@material-ui/core/Button";
+const defaultData = [
+  { key: "4", src: set, name: "세트묵자", price: "20000", qty: 0 },
+  { key: "1", src: burger, name: "햄버거임", price: "6000", qty: 0 },
+  { key: "2", src: chicken, name: "치킨임", price: "17000", qty: 0 },
+  { key: "3", src: coke, name: "콜라임", price: "2500", qty: 0 }
 ];
-
-const productData = [
-  { key: "4", src: set, name: "세트묵자", price: "20000" },
-  { key: "1", src: burger, name: "햄버거임", price: "6000" },
-  { key: "2", src: chicken, name: "치킨임", price: "17000" },
-  { key: "3", src: coke, name: "콜라임", price: "2500" }
-];
-const testPageForDB = props => {
+const TestPageForDB = props => {
+  var [lists, setLists] = useState([]);
   const index = 0;
-  // state = {
-  //   data: []
-  // };
+
+  const addProduct = p => {
+    lists = lists.concat(p);
+    setLists(lists);
+    console.log(lists);
+  };
+  const orderComplete = () => {
+    console.log("주문 완료!");
+  };
   return (
     <Layout>
       <div>
         DB Test page
         <br></br>
         등록페이지...
+        {/* 메뉴들에 대한 정보 defaultData 배열 이미지와 함께 보여주기 */}
         <Table>
           <TableHead>rr</TableHead>
           <TableBody>
-            {products.map(p => {
-              return <Product key={p.key} src={p.src} name={p.name}></Product>;
+            {defaultData.map(p => {
+              return (
+                <TableCell>
+                  <TableRow>
+                    <img
+                      src={p.src}
+                      alt="menu"
+                      style={{ height: 250 }}
+                      onClick={() => addProduct(p)}
+                    />
+                  </TableRow>
+                  <TableRow>
+                    <div>{p.name}</div>
+                  </TableRow>
+                </TableCell>
+              );
             })}
           </TableBody>
         </Table>
+        <div style={{}}>
+          <Table style={{ width: "400px" }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>이름</TableCell>
+                <TableCell>가격</TableCell>
+                <TableCell>수량</TableCell>
+                <Button color="secondary" variant="contained" onClick={() => orderComplete()}>
+                  주문 완료
+                </Button>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* 클릭 이벤트 발생시 해당 메뉴 표에 추가하기 */}
+              {lists.map(({ key, name, price, qty }) => {
+                return (
+                  <TableRow key={key}>
+                    <TableCell>{name}</TableCell>
+                    <TableCell>{price}</TableCell>
+                    <TableCell>{qty}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     </Layout>
   );
 };
-// C:\Users\multicampus\git\s02p12c103\web\src\pages\test\test1_db.jsx
-// C:\Users\multicampus\git\s02p12c103\web\src\img\burger.jpg
-export default testPageForDB;
+
+export default TestPageForDB;
